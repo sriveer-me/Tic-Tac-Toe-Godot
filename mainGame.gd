d6bdbc80b3d17c9baeac6e3874ca7e537182d1bb
Veeru <immaculateempyrean@gmail.com>
2021-11-03 12:34:32 +0530

    almost working.

--- mainGame.gd
+++ mainGame.gd
@@ -8,9 +8,9 @@ func placeCircle(positionX,positionY):
 	gameBoard[(positionY * 3) + positionX] = 1;
 
 #computer piece
-func placeCross(positionX,positionY):
-	gameBoard[(positionY * 3) + positionX] = 2;
-	print('cross placed in %s,%s'%[positionX,positionY]);
+func placeCross(gameBoardPosition):
+	gameBoard[gameBoardPosition] = 2;
+	print('cross placed in %s,%s'%[gameBoardPosition%3,gameBoardPosition/3]);
 
 func isPositionEmpty(positionX,positionY):
 	return gameBoard[(positionY * 3) + positionX] == 0
@@ -53,47 +53,43 @@ func gameOver(gameBoard):
 
 	return 0;
 
-func miniMaxPlay(board,value):
-	var table=[];
-	for casket in table:
-		if(casket["board"] == board):
-			return {"score": casket["score"],"board": casket["board"]};
-	
+var oppositeValue = {
+	"1":2,
+	"2":1
+}
+func miniMaxPlay(board,value,isMax):
 	#start check if the game is over and is the result favourable or not
 	var gameVictor = gameOver(board);
 	if(gameVictor == 2):
-		table.push_front({"score":1,"board":board});
-		return table[0];
+		return {"score" : 1, "move":null};
 	elif(gameVictor == 1):
-		table.push_front({"score":-1,"board":board});
-		return table[0];
+		return {"score" : -1,"move":null};
 	elif(gameBoardFull()):
-		table.push_front({"score":0,"board":board});
-		return table[0];
+		return {"score" : 0, "move":null};
 	#end check if game is over block
 	
-	#start compute who is the opponent
-	var otherValue = 99;
-	if(value == 1):
-		otherValue = 2;
-	else: otherValue = 1;
-	#end compute who is the opponent
-	
-	var highScore = 0;
-	var bestBoard = [];
-	var count = 0;
+	var counter = 0;
+	var highScore = null;
+	var totalScore = 0;
+	var bestMove = null;
 	for point in board:
 		if(point == 0):
-			board[count] = value;
-			var ret = miniMaxPlay(board.duplicate(),otherValue);
-			board[count] = 0
-			if(ret["score"] >= highScore):
-				highScore = ret["score"];
-				bestBoard = ret["board"]
-		count = count + 1;
+			board[counter] = value;
+			var ret = miniMaxPlay(board.duplicate(),oppositeValue[str(value)],!isMax);
+			if(isMax):
+				if(highScore == null or highScore < ret["score"]):
+					highScore=ret["score"];
+					bestMove=counter;
+			else:
+				if(highScore == null or highScore > ret["score"]):
+					highScore=ret["score"];
+					bestMove=counter;
+			totalScore = ret["score"] + totalScore;
+			board[counter] = 0;
+		counter = counter + 1;
 	
-	table.push_front({"score":highScore,"board":bestBoard});
-	return table[0];
+	return {"score":totalScore,"move":bestMove}
+
 
 func getMovement(board1,board2):
 	if(board1.size() != board2.size()):
@@ -108,16 +104,20 @@ func getMovement(board1,board2):
 	return null;
 
 func enemyMove():
-	var newBoard = miniMaxPlay(gameBoard.duplicate(),2);
-	var movement = getMovement(gameBoard,newBoard["board"]);
-	placeCross(movement["x"],movement["y"]);
-	globalFlags.playerCanChoose = true;
+	var newMovement = miniMaxPlay(gameBoard.duplicate(),2,true)["move"];
+	if(newMovement == null):
+		print("Game Over In Tie");
+	else:
+		placeCross(newMovement);
+		globalFlags.playerCanChoose = true;
 
 func PlayerPiecePlaced(position):
 	if(isPositionEmpty(position.x,position.y) != true):
 		print("error, player placed a piece on already occupied slot");
 	placeCircle(position.x,position.y);
-	var isGameOver = gameOver(gameBoard);
-	if(isGameOver):
-		print("game over %s won" % str(isGameOver))
-	else: enemyMove();
+	if(gameOver(gameBoard)):
+			print("game over player won");
+	else: 
+		enemyMove();
+		if(gameOver(gameBoard)):
+			print("game over computer won");
